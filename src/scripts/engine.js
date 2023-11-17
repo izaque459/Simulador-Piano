da50bdbf2706fd377082f929d1c5133456667af5
Isaac_Santos <isaacdsantos@hotmail.com>
2023-11-17 19:13:51 -0300

    Adiciona funcao toggle para as teclas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
 let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
@@ -26,4 +27,8 @@ document.addEventListener("keydown", (e) => {
 
 const handleVolume = (e) => audio.volume = e.target.value;
 
+const showHideKeys = () => pianoKeys.forEach(key=>key.classList.toggle("hide"));
+
 volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys)
